38add91521674b5fd0aa1a14be07b687656b3e0c
Nilanjan Bhattacharjee <nilanjan.bhattacharjee77@gmail.com>
2023-10-27 11:21:10 +0530

    added literals

---
+++ src/literals.py
@@ -0,0 +1,30 @@
+a = 1e9
+
+print(a)
+print(int(a))
+
+b = 0o123 # octal number
+
+print(b)
+
+c = 0x123 # hexadecimal number
+
+print(c)
+
+d = 10.51
+
+print(type(d))
+
+
+e = 1_000_000 # more readable
+
+print(e)
+
+''' Output: 
+1000000000.0
+1000000000
+83
+291
+<class 'float'>
+1000000
+'''
